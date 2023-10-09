75f5e4643e614453e10fe57c975eda19445daee9
Your Name <kkamadi567@gmail.com>
2023-10-09 10:35:49 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your code here!
+// // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "MORINGA is the champion"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ADDING ELEMENTS TO INNER DOM VIA innerHTML
+// // // const element = document.createElement("div");
+
+// "<h1 id ="Kevin Kamadi is the Champion"></h1>
+// // CHANGE PROPERTIES ON DOM nodes
+
+// // const element = document.getElementById("main");
+// // element.style.height = "400px";
+// // element.style.backgroundColor = "#27647B";
+// // element.style.fontSize = "24px";
+// // element.style.marginLeft = "30px";
+// // element.style.lineHeight = 2;
+// element.textContent = "Kevin Mahindu is the champion ";
+// // element.className = "pet-listing dog";
+
+
+//     //  remove elements test
+    
